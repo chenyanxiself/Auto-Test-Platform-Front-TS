ac7cc23a917cb0f7da6a20ab28f2930f58cb4a77
Chen,Eric <CHENY7@legal.regn.net>
2021-03-09 16:00:55 +0800

    fix

--- src/layouts/components/TopHeader.tsx
+++ src/layouts/components/TopHeader.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Menu, Avatar, Popover, Button } from 'antd';
+import { Menu, Avatar, Popover, Button, Dropdown } from 'antd';
 import styles from './topHeader.less';
 import ProjectMenusList from './ProjectMenusList';
-import { connect,withRouter } from 'umi';
+import { connect, withRouter } from 'umi';
 import storeageUtil, { localStorageKey } from '@/utils/storageUtil';
+import {
+  UserOutlined,
+  SettingOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons';
 
-const TopHeader = (props) => {
-  let selectedItem = ProjectMenusList.find(item => (item.regExp.test(props.history.location.pathname)));
+const TopHeader = props => {
+  let selectedItem = ProjectMenusList.find(item =>
+    item.regExp.test(props.history.location.pathname),
+  );
   let isShowMenu = !!selectedItem;
   let key = selectedItem ? [selectedItem.path] : [];
   let { cname } = props.global;
-  const getProjectIdByPath = (path) => {
+  const getProjectIdByPath = path => {
     const regExp = /^\/project\/(.+?)\/\w+/;
     if (regExp.test(path)) {
       return path.match(regExp)[1];
@@ -18,12 +25,8 @@ const TopHeader = (props) => {
       return null;
     }
   };
-  const handleUserInfo = () => {
-    if (props.history.location.pathname !== `/user/info`) {
-      props.history.push(`/user/info`);
-    }
-  };
-  const onMenuClick = (item) => {
+
+  const onMenuClick = item => {
     let { key } = item;
     const projectId = getProjectIdByPath(props.history.location.pathname);
     const url = key.replace(':id', projectId);
@@ -31,24 +34,37 @@ const TopHeader = (props) => {
       props.history.push(url);
     }
   };
-  const logout = () => {
-    storeageUtil.remove(localStorageKey.TOKEN);
-    props.history.push('/login/');
+
+  const userMenuClickHandler = ({ key }) => {
+    if (key == 'center') {
+      props.history.push(`/user/info`);
+    } else if (key == 'logout') {
+      storeageUtil.remove(localStorageKey.TOKEN);
+      props.history.push('/login/');
+    }
   };
 
   const content = (
-    <>
-      <div>
-        <Button type="link" onClick={handleUserInfo}>个人信息</Button>
-      </div>
-      <div>
-        <Button type="link" onClick={logout}>退出</Button>
-      </div>
-    </>
+    <Menu
+      className={styles.menu}
+      selectedKeys={[]}
+      onClick={userMenuClickHandler}
+    >
+      <Menu.Item key="center">
+        <UserOutlined />
+        个人中心
+      </Menu.Item>
+      <Menu.Divider />
+
+      <Menu.Item key="logout">
+        <LogoutOutlined />
+        退出登录
+      </Menu.Item>
+    </Menu>
   );
   return (
     <div className={styles.main}>
-      {isShowMenu ?
+      {isShowMenu ? (
         <Menu
           className={styles.menus}
           theme="light"
@@ -58,20 +74,22 @@ const TopHeader = (props) => {
         >
           {ProjectMenusList.map(item => {
             return (
-              <Menu.Item key={item.path} icon={item.icon}>{item.name}</Menu.Item>
+              <Menu.Item key={item.path} icon={item.icon}>
+                {item.name}
+              </Menu.Item>
             );
           })}
         </Menu>
-        : null}
+      ) : null}
       <div className={styles.user}>
-        <Popover
-          content={content}
-          placement="bottom"
-        >
-          <Avatar className={styles.avatar} size='default'>
-            {cname ? cname.substring(cname.length - 2, cname.length) : null}
-          </Avatar>
-        </Popover>
+        <Dropdown overlay={content} placement={'bottomCenter'}>
+          <span className={styles.account}>
+            <Avatar className={styles.avatar} size="default">
+              {cname ? cname.substring(cname.length - 2, cname.length) : null}
+            </Avatar>
+            <span className={styles.name}>{cname}</span>
+          </span>
+        </Dropdown>
       </div>
     </div>
   );
